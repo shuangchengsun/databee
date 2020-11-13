d8c82984a371736b6d41414fb165bd4d986a2342
sunshuangcheng <shuangchengsun@163.com>
2020-11-14 00:30:53 +0800

    修复了组件多class的异常

--- src/main/java/com/alan/databee/service/TaskService.java
+++ src/main/java/com/alan/databee/service/TaskService.java
@@ -9,6 +9,7 @@ import com.alan.databee.model.CommonConfig;
 import com.alan.databee.model.DebugResult;
 import com.alan.databee.model.ResultEnum;
 import com.alan.databee.spider.exception.ScriptException;
+import com.alan.databee.spider.exception.SpiderErrorEnum;
 import com.alan.databee.spider.model.SpiderTaskConfig;
 import com.alan.databee.spider.model.User;
 import com.alan.databee.spider.script.ScriptService;
@@ -61,6 +62,7 @@ public class TaskService {
             result.setTime(0);
             result.setDownloadStat(ResultEnum.Result_Undefined.getStatMsg());
             result.setPipelineStat(ResultEnum.Result_Undefined.getStatMsg());
+            return result;
         }
         // 2、检查组件是否使用了默认的组件名字，并检查组件是否能够通过编译
         String downloader = model.getDownloader();
@@ -95,80 +97,91 @@ public class TaskService {
             return result;
 
         // 组装数据
-        SpiderConfigDao configDao = AssemblyDao(model,user);
+        SpiderConfigDao configDao = AssemblyDao(model, user);
         ComponentConfigDao componentConfigDao = new ComponentConfigDao();
         componentConfigDao.setName(configDao.getComponentConfig());
         componentConfigDao.setVersion(1);
         List<ComponentDao> componentDaoList = new ArrayList<>();
-        if(downloader != null && downloader.length()>32){
-            ComponentDao downloadComponentDao = new ComponentDao();
-            String comName = getComName(downloader);
-            comName = user.getUserName()+comName;
-            if(comName .length() > 32){
-                comName = comName.substring(0,32);
-            }
-            downloadComponentDao.setName(comName);
-            downloadComponentDao.setContent(downloader);
-            componentDaoList.add(downloadComponentDao);
-            componentConfigDao.setDownloader(comName);
-        }else if(downloader != null){
-            componentConfigDao.setDownloader(downloader);
-        }
-
-        // 目前，只支持一个
-        for(String processor:pageProcessor){
-            ComponentDao processorDao = new ComponentDao();
-            String comName = getComName(processor);
-            comName = user.getUserName()+comName;
-            if(comName .length() > 32){
-                comName = comName.substring(0,32);
-            }
-            processorDao.setName(comName);
-            processorDao.setContent(processor);
-            componentDaoList.add(processorDao);
-            componentConfigDao.setPageProcessor(comName);
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for(String pipeline : pipelines){
-            if(pipeline!=null && pipeline.length()>32) {
-                ComponentDao pipelineDao = new ComponentDao();
-                String comName = getComName(pipeline);
+        try {
+            if (downloader != null && downloader.length() > 32) {
+                ComponentDao downloadComponentDao = new ComponentDao();
+                String comName = getComName(downloader);
                 comName = user.getUserName() + comName;
                 if (comName.length() > 32) {
                     comName = comName.substring(0, 32);
                 }
-                pipelineDao.setName(comName);
-                pipelineDao.setContent(pipeline);
-                componentDaoList.add(pipelineDao);
-                stringBuilder.append(comName).append(",");
-            }else if(pipeline != null){
-                stringBuilder.append(pipeline).append(",");
+                downloadComponentDao.setName(comName);
+                downloadComponentDao.setContent(downloader);
+                componentDaoList.add(downloadComponentDao);
+                componentConfigDao.setDownloader(comName);
+            } else if (downloader != null) {
+                componentConfigDao.setDownloader(downloader);
             }
-        }
-        String pipelineName = stringBuilder.deleteCharAt(stringBuilder.length()).toString();
-        componentConfigDao.setPipelines(pipelineName);
 
-        if(scheduler!=null && scheduler.length()>32){
-            ComponentDao schedulerDao = new ComponentDao();
-            String comName = getComName(scheduler);
-            comName = user.getUserName() + comName;
-            if (comName.length() > 32) {
-                comName = comName.substring(0, 32);
+            // 目前，只支持一个
+            for (String processor : pageProcessor) {
+                ComponentDao processorDao = new ComponentDao();
+                String comName = getComName(processor);
+                comName = user.getUserName() + comName;
+                if (comName.length() > 32) {
+                    comName = comName.substring(0, 32);
+                }
+                processorDao.setName(comName);
+                processorDao.setContent(processor);
+                componentDaoList.add(processorDao);
+                componentConfigDao.setPageProcessor(comName);
             }
-            schedulerDao.setName(comName);
-            schedulerDao.setContent(scheduler);
-            componentConfigDao.setSchedule(comName);
-            componentDaoList.add(schedulerDao);
-        }else if(scheduler!=null){
-            componentConfigDao.setSchedule(scheduler);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String pipeline : pipelines) {
+                if (pipeline != null && pipeline.length() > 32) {
+                    ComponentDao pipelineDao = new ComponentDao();
+                    String comName = getComName(pipeline);
+                    comName = user.getUserName() + comName;
+                    if (comName.length() > 32) {
+                        comName = comName.substring(0, 32);
+                    }
+                    pipelineDao.setName(comName);
+                    pipelineDao.setContent(pipeline);
+                    componentDaoList.add(pipelineDao);
+                    stringBuilder.append(comName).append(",");
+                } else if (pipeline != null) {
+                    stringBuilder.append(pipeline).append(",");
+                }
+            }
+            String pipelineName = stringBuilder.deleteCharAt(stringBuilder.length()).toString();
+            componentConfigDao.setPipelines(pipelineName);
+
+            if (scheduler != null && scheduler.length() > 32) {
+                ComponentDao schedulerDao = new ComponentDao();
+                String comName = getComName(scheduler);
+                comName = user.getUserName() + comName;
+                if (comName.length() > 32) {
+                    comName = comName.substring(0, 32);
+                }
+                schedulerDao.setName(comName);
+                schedulerDao.setContent(scheduler);
+                componentConfigDao.setSchedule(comName);
+                componentDaoList.add(schedulerDao);
+            } else if (scheduler != null) {
+                componentConfigDao.setSchedule(scheduler);
+            }
+            // 写入数据库
+            configService.saveSpiderConfig(configDao);
+            configService.saveComponentConfig(componentConfigDao);
+            for (ComponentDao componentDao : componentDaoList) {
+                configService.saveComponent(componentDao);
+            }
+            result = new DebugResult();
+            result.setStat(SUCCESS_STAT);
+            result.setMsg("任务提交成功");
+        } catch (ScriptException exception) {
+            result = new DebugResult();
+            result.setStat(ResultEnum.Component_Compiler_Error.getStatCode());
+            result.setMsg("在编译时找到了多个class");
+
         }
-        // 写入数据库
-        configService.saveSpiderConfig(configDao);
-        configService.saveComponentConfig(componentConfigDao);
-        for(ComponentDao componentDao:componentDaoList){
-            configService.saveComponent(componentDao);
-        }
+
         return result;
     }
 
@@ -199,14 +212,14 @@ public class TaskService {
         return result;
     }
 
-    private String getComName(String com) {
-        Pattern pattern = Pattern.compile("^public(?<type>\\w+|\\s)class (?<name>\\w+)");
+    private String getComName(String com) throws ScriptException {
+        Pattern pattern = Pattern.compile("^public(?<type>\\w+|\\s+)class (?<name>\\w+)");
         Matcher matcher = pattern.matcher(com);
         int find = 0;
         String name = null;
         while (matcher.find()) {
             if (find >= 1) {
-                return null;
+                throw new ScriptException(SpiderErrorEnum.Script_ClassNum_Error);
             }
             find++;
             name = matcher.group("name");
@@ -216,19 +229,19 @@ public class TaskService {
 
     private DebugResult checkComCompile(String com) {
         DebugResult result = null;
-        String comName = getComName(com);
         try {
+            String comName = getComName(com);
             scriptService.genClass(comName, com);
         } catch (ScriptException exception) {
             result = buildError(ResultEnum.Component_Compiler_Error);
-            result.addExtMsg("ErrMsg", "组件的代码编译未通过，组件名: " + comName);
+            result.addExtMsg("ErrMsg", "组件的代码编译未通过，组件名: " + com);
         }
         return result;
     }
 
     private SpiderConfigDao AssemblyDao(BusyReqModel model, User user) {
         SpiderConfigDao configDao = new SpiderConfigDao();
-        if(model.getBusyCode() == SUBMIT_NEW) {
+        if (model.getBusyCode() == SUBMIT_NEW) {
             configDao.setCreator(user.getUserName());
 
             long l = System.currentTimeMillis();
--- src/main/java/com/alan/databee/spider/exception/SpiderErrorEnum.java
+++ src/main/java/com/alan/databee/spider/exception/SpiderErrorEnum.java
@@ -7,7 +7,9 @@ public enum SpiderErrorEnum {
     Unsupported_Operation("不支持的操作"),
     Class_Instance_Error("类实例化错误"),
     Class_Init_Error("类初始化错误，注入参数错误"),
-    Component_Is_Empty("组件内容为空")
+    Component_Is_Empty("组件内容为空"),
+    Script_ClassNum_Error("脚本格式错误，解析到的class多于一个"),
+    Script_ClassType_Error("脚本格式错误，解析到了抽象的class")
     ;
     String msg;
 
